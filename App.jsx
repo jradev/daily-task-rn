ead04512bfe9e285bf95b8824144fafd9ea080e4
Jomari Iringan <jomari.iringan@legalmatch.com>
2023-08-25 11:18:18 +0800

    chore: install react navigation & rename root app

---
+++ App.jsx
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(props) {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
+
+export default App;
