30dee6feae230b60f7b6790bdfc57b4f019908e2
Jomari Iringan <jomari.iringan@legalmatch.com>
2023-08-30 13:04:39 +0800

    chore: linter fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {persistStore, persistReducer} from 'redux-persist';
 
 import appReducer from '@app-redux/reducer';
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 
 const persistConfig = {
   key: 'root',
@@ -13,6 +13,7 @@ const persistedReducer = persistReducer(persistConfig, appReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false,}),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({serializableCheck: false}),
 });
 export const persistor = persistStore(store);
