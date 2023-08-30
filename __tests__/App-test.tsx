5b61aff1eef584d6f39c263f3401a59797594da6
Jomari Iringan <jomari.iringan@legalmatch.com>
2023-08-30 14:34:18 +0800

    chore: linter changes

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,22 +4,26 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import { FlatList, Text } from 'react-native';
+import {FlatList, Text} from 'react-native';
 
 it('renders correctly', () => {
-  const tasks = [{id: 'AAB', title: 'Create Mobile App'}, {id: 'BBA', title: 'Update Mobile App'}];
-  const tree = renderer.create(
-    <FlatList
-    data={tasks ?? []}
-    renderItem={({item}) => <Text>{item.title}</Text>}
-    keyExtractor={(item) => `task_id_${item?.id}`}
-    extraData={tasks}
-    />
-  ).toJSON();
+  const tasks = [
+    {id: 'AAB', title: 'Create Mobile App'},
+    {id: 'BBA', title: 'Update Mobile App'},
+  ];
+  const tree = renderer
+    .create(
+      <FlatList
+        data={tasks ?? []}
+        renderItem={({item}) => <Text>{item.title}</Text>}
+        keyExtractor={item => `task_id_${item?.id}`}
+        extraData={tasks}
+      />,
+    )
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
