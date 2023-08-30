8e871ad219e591f2f2eff7c0bae7a62af1ef2e35
Jomari Iringan <jomari.iringan@legalmatch.com>
2023-08-30 14:27:39 +0800

    chore: flatlist previous test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -8,7 +8,18 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { FlatList, Text } from 'react-native';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tasks = [{id: 'AAB', title: 'Create Mobile App'}, {id: 'BBA', title: 'Update Mobile App'}];
+  const tree = renderer.create(
+    <FlatList
+    data={tasks ?? []}
+    renderItem={({item}) => <Text>{item.title}</Text>}
+    keyExtractor={(item) => `task_id_${item?.id}`}
+    extraData={tasks}
+    />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
